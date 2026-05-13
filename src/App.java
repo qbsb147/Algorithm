8c83f5aea2079d371c599fa0c4665058a9692f7c
DESKTOP-OFUV93T\USER <chance787@naver.com>
2026-05-13 22:51:30 +0900

    init

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import sort.BubbleSort;
+import sort.BubbleSortBase;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        BubbleSort bubbleSort = new BubbleSortBase();
+        bubbleSort.run();
+    }
+}
